242644b36c119ef12673b361c2d775f187b3cbbc
Amy Wong <Amy Wong@WINTER>
2018-11-16 18:51:17 -0500

    adding files

---
+++ binaryselection.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int likePizza = 0;
+
+	if (likePizza == 1) {
+		printf("I like pizza\n");
+	}
+	else {
+		printf("I hate pizza\n");
+		printf("I don't want pizza\n");
+	}
+	return 0;
+}
+
