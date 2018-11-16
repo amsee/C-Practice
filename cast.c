f8ed471d0bbd7c9990508941cb9b11e9ea113525
Amy Wong <Amy Wong@WINTER>
2018-11-16 16:00:23 -0500

    adding files

---
+++ cast.c
@@ -0,0 +1,17 @@
+// From minutes to hours
+// cast.c
+define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int minutes;
+	float hours;
+
+	printf("Minutes ? ");
+	scanf("%d", &minutes);
+	hours = (float)minutes / 60;
+	printf("= %.2lf hours\n", hours);
+
+	return 0;
+}
