50da147d642695a471d57ad8d2d3aa4ff5ce365e
Amy Wong <Amy Wong@WINTER>
2018-11-16 21:49:17 -0500

    adding files

---
+++ case.c
@@ -0,0 +1,49 @@
+// Case-by-Case Selection
+// case.c
+
+/*
+switch (condition) {
+case constant:
+		sequence
+		break;
+case constant:
+		sequence
+		break;
+default:
+		sequence
+}
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char choice;
+	double cost;
+
+	printf("Enter your selection (a, b or c) ? ");
+	scanf("%c", &choice);
+
+	switch (choice) {
+	case 'A':
+	case 'a':
+		cost = 1.50;
+		break;
+	case 'B':
+	case 'b':
+		cost = 1.10;
+		break;
+	case 'C':
+	case 'c':
+		cost = 0.75;
+		break;
+	default:
+		choice = '?';
+		cost = 0.0;
+	}
+
+	printf("%c costs %.2lf\n", choice, cost);
+
+	return 0;
+
+}
