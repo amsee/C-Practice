1faa3abb9b7580ea5922757f445840173b4952d4
Amy Wong <Amy Wong@WINTER>
2018-11-19 18:36:21 -0500

    adding files

---
+++ length.c
@@ -0,0 +1,20 @@
+// Layout: Line Length 
+// length.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d substrings"
+               " display as a"
+               " single string" 
+               "\n", 3);
+
+	return 0;
+}
+
+/* OUTPUT
+
+ 3 substrings display as a single string 
+
+*/
